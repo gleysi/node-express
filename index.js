6b8535985b85061a08fbaa985c611b53d65c3017
Miranda Tejada <sonia.miranda.tejada@accenture.com>
2020-06-19 15:18:00 -0500

    Express Router

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
-const express = require('express'),
-     http = require('http');
+const express = require('express');
+const http = require('http');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+
+const dishRouter = require('./routes/dishRouter');
 
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
 app.use(morgan('dev'));
-
 app.use(bodyParser.json());
+
+app.use('/dishes', dishRouter);
+
+app.use(express.static(__dirname + '/public'));
 
 app.all('/dishes', (req,res,next) => {
   res.statusCode = 200;
@@ -53,7 +58,6 @@ app.delete('/dishes/:dishId', (req, res, next) => {
     res.end('Deleting dish: ' + req.params.dishId);
 });
 
-app.use(express.static(__dirname + '/public'));
 
 app.use((req, res, next) => {
   //console.log(req.headers);
